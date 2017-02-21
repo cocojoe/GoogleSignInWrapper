e4a59a57ce49360ebafd4f06c4ea7752255001d9
Martin Walsh <martin.walsh@gmail.com>
2017-02-21 10:03:22 +0000

    Foundation Build

---
+++ GoogleSignInWrapper/GoogleSignInWrapper.h
@@ -0,0 +1,19 @@
+//
+//  GoogleSignInWrapper.h
+//  GoogleSignInWrapper
+//
+//  Created by Martin Walsh on 20/02/2017.
+//  Copyright © 2017 Auth0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleSignInWrapper.
+FOUNDATION_EXPORT double GoogleSignInWrapperVersionNumber;
+
+//! Project version string for GoogleSignInWrapper.
+FOUNDATION_EXPORT const unsigned char GoogleSignInWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleSignInWrapper/PublicHeader.h>
+
+#import "GoogleSignIn.h"
