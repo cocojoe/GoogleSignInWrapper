dc4b0de36a9fa380fd27652438036de3ef0249d4
Martin Walsh <martin.walsh@gmail.com>
2017-02-21 11:47:31 +0000

    Update

--- GoogleSignInWrapper/GoogleSignInWrapper.h
+++ GoogleSignInWrapper/GoogleSignInWrapper.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double GoogleSignInWrapperVersionNumber;
 FOUNDATION_EXPORT const unsigned char GoogleSignInWrapperVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GoogleSignInWrapper/PublicHeader.h>
-
-#import "GoogleSignIn.h"
